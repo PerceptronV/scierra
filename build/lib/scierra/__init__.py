90e167228a5f291d0ce490dab6b905a99a4aed82
PerceptronV <neutrinovs@gmail.com>
2020-12-15 16:25:40 +0800

    Build data

---
+++ build/lib/scierra/__init__.py
@@ -0,0 +1,6 @@
+from scierra import compile
+from scierra import utils
+from scierra.sim import Simulator
+
+import os
+import random
---
+++ build/lib/scierra/scierra.py
@@ -0,0 +1,40 @@
+from scierra.sim import Simulator
+
+def main():
+    flag = True
+    res = True
+    simul = Simulator()
+
+    print('Scierra C++ Interpreter\nCopyright (c) PerceptronV 2020')
+    line = input('\n++> ')
+    stripline = line.strip()
+
+    if stripline == '<print>':
+        simul.print_code()
+    elif stripline == '<restart>':
+        simul.restart()
+    elif stripline == '<esc>':
+        flag = False
+    elif stripline == '':
+        pass
+    else:
+        res = simul.addline(line + '\n')
+
+    while (flag):
+        if res:
+            line = input('\n++> ')
+        else:
+            line = input('\n--> ')
+        stripline = line.strip()
+
+        if stripline == '<print>':
+            simul.print_code()
+        elif stripline == '<restart>':
+            res = True
+            simul.restart()
+        elif stripline == '<esc>':
+            flag = False
+        elif stripline == '':
+            pass
+        else:
+            res = simul.addline(line + '\n')
