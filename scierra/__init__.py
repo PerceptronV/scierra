6c3b93c83e08aea3758e7c88ec5c5efc5ebb7a76
PerceptronV <neutrinovs@gmail.com>
2020-12-15 15:09:09 +0800

    Update __init__.py

--- scierra/__init__.py
+++ scierra/__init__.py
@@ -1,1 +1,3 @@
 from scierra.sim import Simulator
+from scierra.compile import compile
+from scierra.utils import rndstr, gen_name, working_dir, count, skip_whitespaces, count_fors, empty_couts, replace_string, replace_comment
